5705f730b20d4367896f05cd78f978ed7e3216ea
PradeepShunmugam <pradeep.shunmugam@protonmail.com>
2026-04-17 12:21:50 +0530

    added csv reader in code

--- cli/cmd/add.go
+++ cli/cmd/add.go
@@ -1,7 +1,9 @@
 package cmd
 
 import (
+	"encoding/csv"
 	"fmt"
+	"os"
 	"scheduler/cli/cmd/dbconn"
 
 	"github.com/spf13/cobra"
@@ -12,19 +14,36 @@ var name string
 var cron string
 var sample string
 var email string
+var csvFile string
 
 var addCmd = &cobra.Command{
 	Use:   "add",
 	Short: "add the url",
 	Long:  "To monitor or initialize the URL, add it using 'sch add -flag <> '. Add url in format - 'https://website.com'. ",
 	Run: func(cmd *cobra.Command, args []string) {
-		if url == "" && name == "" {
-			fmt.Println("Please provide url and name. Provided is empty")
-
-		} else {
+		if url != "" && name != "" {
 			fmt.Printf("got the url : %v and name %v to monitor.", url, name)
 			dbconn.DBInsert(name, url) //Once get the url and respective details from user. call the function to uopdate in  DB - scheduler
 		}
+		if csvFile != "" {
+			file, err := os.Open(csvFile)
+			if err != nil {
+				fmt.Println("Unable to read file.")
+				return
+			}
+			reader := csv.NewReader(file)
+			records, err := reader.ReadAll()
+			if err != nil {
+				fmt.Println(err)
+			}
+			for i := range records {
+				name, url := records[i][0], records[i][1]
+				dbconn.DBInsert(name, url)
+			}
+			fmt.Println("Data inserted")
+
+		}
+		fmt.Println("Please provide url and name. Provided is empty")
 
 	},
 }
@@ -36,4 +55,5 @@ func init() {
 	addCmd.Flags().StringVarP(&cron, "cron", "c", "", "cron definition to check the url")
 	addCmd.Flags().StringVarP(&sample, "sample", "s", "", "number of samples to check")
 	addCmd.Flags().StringVarP(&email, "email", "m", "", "email id to send notification")
+	addCmd.Flags().StringVarP(&csvFile, "file", "g", "", "csv file to add bulk data.")
 }
