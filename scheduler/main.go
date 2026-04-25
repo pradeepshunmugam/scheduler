3849364c5e64951c9b0f01242b3a9169212f389d
PradeepShunmugam <pradeep.shunmugam@protonmail.com>
2026-04-25 10:06:34 +0530

    added checking the status

--- scheduler/main.go
+++ scheduler/main.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"database/sql"
+	"fmt"
+	"os"
 	"scheduler/logger"
 	"scheduler/scheduler/scheduler"
+	"sync"
 
 	"github.com/joho/godotenv"
 	"go.uber.org/zap"
@@ -16,5 +20,30 @@ func main() {
 		logger.Log.Error("Unable to load environment. ", zap.Error(err))
 		return
 	}
-	scheduler.ReadURL()
+	var wg sync.WaitGroup
+	wg.Add(2)
+	dbhost := os.Getenv("DB_HOST")
+	dbport := os.Getenv("DB_PORT")
+	dbuser := os.Getenv("DB_USER")
+	dbpassword := os.Getenv("DB_PASSWORD")
+	dbname := os.Getenv("DB_NAME_SCHEDULER")
+
+	connString := fmt.Sprintf(`host = %v port = %v dbname = %v user = %v password = %v sslmode = disable`, dbhost, dbport, dbname, dbuser, dbpassword)
+	dbconn, err := sql.Open("postgres", connString)
+	if err != nil {
+		logger.Log.Error("Unable to connect to DB to read the urls!!, ", zap.Error(err))
+	}
+	defer func() {
+		if r := recover(); r != nil {
+			fmt.Println("Recovered from panic")
+		}
+	}()
+	if err != nil {
+		logger.Log.Error("Unableto read the urls!!, ", zap.Error(err))
+		panic(err)
+	}
+	go scheduler.UpdateNextRun(dbconn, &wg)
+	go scheduler.Run(dbconn, &wg)
+	wg.Wait()
+
 }
--- scheduler/scheduler/schedule.go
+++ scheduler/scheduler/schedule.go
@@ -2,11 +2,15 @@ package scheduler
 
 import (
 	"database/sql"
+	"encoding/json"
 	"fmt"
-	"os"
+	"net/http"
 	"scheduler/logger"
+	"sync"
+	"time"
 
 	_ "github.com/lib/pq"
+	"github.com/robfig/cron/v3"
 	"go.uber.org/zap"
 )
 
@@ -20,41 +24,94 @@ type Jobs struct {
 	next_run string
 }
 
-func ReadURL() {
-	dbhost := os.Getenv("DB_HOST")
-	dbport := os.Getenv("DB_PORT")
-	dbuser := os.Getenv("DB_USER")
-	dbpassword := os.Getenv("DB_PASSWORD")
-	dbname := os.Getenv("DB_NAME_SCHEDULER")
-
-	connString := fmt.Sprintf(`host = %v port = %v dbname = %v user = %v password = %v sslmode = disable`, dbhost, dbport, dbname, dbuser, dbpassword)
-	dbconn, err := sql.Open("postgres", connString)
+func UpdateNextRun(db *sql.DB, wg *sync.WaitGroup) {
+	defer wg.Done()
+	rows, err := db.Query("SELECT name, url, sample, cron, email, last_run, next_run FROM url_list;")
 	if err != nil {
 		logger.Log.Error("Unable to connect to DB to read the urls!!, ", zap.Error(err))
-	}
-	rows, err := dbconn.Query("SELECT name, url, sample, cron, email, last_run, next_run FROM url_list;")
-	defer func() {
-		if r := recover(); r != nil {
-			fmt.Println("Recovered from panic")
-		}
-	}()
-	if err != nil {
-		logger.Log.Error("Unableto read the urls!!, ", zap.Error(err))
-		panic(err)
 	}
 	for rows.Next() {
 		var job Jobs
 		err := rows.Scan(&job.name, &job.url, &job.sample, &job.cron, &job.email, &job.last_run, &job.next_run)
 		defer rows.Close()
 		if err != nil {
-			logger.Log.Error("Unableto scan the urls!!, ", zap.Error(err))
+			logger.Log.Error("Unable to scan the urls!!, ", zap.Error(err))
 		}
-		log(job)
+		parser := cron.NewParser(cron.Minute | cron.Hour | cron.Dom | cron.Month | cron.Dow)
+		schedule, err := parser.Parse(job.cron)
+		if err != nil {
+			fmt.Println("Unable to parse the time. ", err)
+		}
+
+		//Inser next run in db.
+
+		next := schedule.Next(time.Now())
+		currrentTime := time.Now()
+		query := (`UPDATE url_list SET next_run = $1 where name = $2`)
+		_, err = db.Exec(query, next, job.name)
+		if err != nil {
+			logger.Log.Error(`Unable to insert the next_run.`, zap.Error(err))
+		} else {
+			logger.Log.Info("next run is updated. ", zap.Time("current time :", currrentTime), zap.String("url name : ", job.name))
+		}
 	}
+}
+
+func Run(db *sql.DB, wg *sync.WaitGroup) {
+	defer wg.Done()
+	i := 0
+	var scheduleList []map[string](interface{})
+	query := `SELECT name, url, next_run, sample FROM url_list;`
+	rows, err := db.Query(query)
+	if err != nil {
+		fmt.Println(err)
+	}
+	var job Jobs
+	for rows.Next() {
+		err := rows.Scan(&job.name, &job.url, &job.next_run, &job.sample)
+		if err != nil {
+			fmt.Println(err)
+		}
+		parsedNextRun, err := time.Parse(time.RFC3339, job.next_run)
+		currrentTime := time.Now()
+		diff := parsedNextRun.Sub(currrentTime)
+		minutesPart := int(diff.Minutes()) % 60
+		if minutesPart < 5 {
+			i++
+			scheduleMap := map[string]interface{}{
+				"name":   job.name,
+				"url":    job.url,
+				"sample": job.sample,
+			}
+			scheduleList = append(scheduleList, scheduleMap)
+		}
+	}
+	data, err := json.Marshal(scheduleList)
+	if err != nil {
+		logger.Log.Error("failed to marshal scheduleList")
+		return
+	}
+	logger.Log.Info("Going to schedule  ", zap.Int("total : ", i), zap.String("schedule list : ", string(data)))
+
+	checkStatus(scheduleList)
 
 }
-func log(job Jobs) {
-	//logger.Log.Info("Read", zap.String("name", job.name), zap.String("url", job.url), zap.String("cron", job.cron), zap.String("email", job.email), zap.String("last_run", job.last_run), zap.String("next_run", job.next_run))
 
-	calculateNextRun(job)
+func checkStatus(scheduleList []map[string](interface{})) {
+	defer func() {
+		if r := recover(); r != nil {
+			fmt.Println("Recovered")
+		}
+	}()
+	for i := range scheduleList {
+		url := fmt.Sprint(scheduleList[i]["url"])
+		urlStatus, err := http.Get(url)
+		if err != nil {
+			fmt.Println(err)
+			panic("oops something happened !!! Panic")
+		}
+		fmt.Printf("Url status of %v is %v.\n", url, urlStatus.StatusCode)
+
+	}
+
 }
