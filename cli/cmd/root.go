ad6ec50caa12385de945205d745b9d3c466574b0
PradeepShunmugam <pradeep.shunmugam@protonmail.com>
2026-05-17 17:10:34 +0530

    created seperate for user validation and calling it from loginCmd

--- cli/cmd/root.go
+++ cli/cmd/root.go
@@ -22,9 +22,9 @@ to quickly create a Cobra application.`,
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
 	// Run: func(cmd *cobra.Command, args []string) { },
-	PersistentPreRun: func(cmd *cobra.Command, args []string) {
-		ValidUser()
-	},
+	// PersistentPreRun: func(cmd *cobra.Command, args []string) {
+	// 	ValidUser()
+	// },
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
